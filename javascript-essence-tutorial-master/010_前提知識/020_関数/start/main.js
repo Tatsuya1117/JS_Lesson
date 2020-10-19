83caf4ceaa9d12f7124251f89b0c4a4557bd0a94
TatsuyaOzawa <othe.world.pc@gmail.com>
2020-10-19 22:48:09 +0900

    変数、関数について

---
+++ javascript-essence-tutorial-master/010_前提知識/020_関数/start/main.js
@@ -0,0 +1,16 @@
+let name = 'Tim';
+
+function hello(name) {
+    console.log('hello ' + name);
+}
+
+hello(name);
+
+let obj = {
+    property1: 'Hello',
+    property2: function() {
+    },
+    property3: {
+        d: 'Bye'
+    }
+}
